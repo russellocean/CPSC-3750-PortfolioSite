98a761d7551fdd2296dead8004bd82a350b73eea
Russell Welch <russellwelch17@gmail.com>
2024-02-03 18:14:39 -0500

    Update navbar links to use absolute paths

--- src/navbar/navbar-component.js
+++ src/navbar/navbar-component.js
@@ -3,15 +3,15 @@ class NavbarComponent extends HTMLElement {
     super();
     this.attachShadow({ mode: "open" });
     this.shadowRoot.innerHTML = `
-    <link rel="stylesheet" href="src/navbar/navbar.css">
+    <link rel="stylesheet" href="/src/navbar/navbar.css">
     <nav id="navbar">
           <ul>
-            <li><a href="index.html">HOME</a></li>
-            <li><a href="src/pages/about.html">ABOUT ME</a></li>
-            <li><a href="src/pages/projects.html">PROJECTS</a></li>
-            <li><a href="src/MoreCSS/GroupCSS2.html">MORE CSS PROJECT</a></li>
-            <li><a href="src/BasicFixedFluidHybridLayout/layout.html">FLUID HYBRID LAYOUT</a></li>
-            <li><a href="mailto:russellwelch17@gmail.com">CONTACT</a></li>
+            <li><a href="/index.html" data-page="home">HOME</a></li>
+            <li><a href="/src/pages/about.html" data-page="about">ABOUT ME</a></li>
+            <li><a href="/src/pages/projects.html" data-page="projects">PROJECTS</a></li>
+            <li><a href="/src/MoreCSS/GroupCSS2.html" data-page="morecss">MORE CSS PROJECT</a></li>
+            <li><a href="/src/BasicFixedFluidHybridLayout/layout.html" data-page="layout">FLUID HYBRID LAYOUT</a></li>
+            <li><a href="mailto:russellwelch17@gmail.com" data-page="contact">CONTACT</a></li>
           </ul>
         </nav>
       `;
@@ -20,7 +20,7 @@ class NavbarComponent extends HTMLElement {
   connectedCallback() {
     const currentPage = this.getAttribute("current-page");
     this.shadowRoot.querySelectorAll("a").forEach((link) => {
-      if (link.getAttribute("href") === currentPage) {
+      if (link.dataset.page === currentPage) {
         link.classList.add("active");
       }
     });
