0f792df11abaaf3696c961f4ad786bff2dd02082
russellocean <russellwelch17@gmail.com>
2024-02-06 22:39:57 -0500

    Add PHP Hello World page with mixed HTML and PHP code

---
+++ src/PHPHelloWorld/helloworld.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>PHP Test</title>
+    <script src="/src/navbar/navbar-component.js" defer></script>
+</head>
+<body>
+    <header>
+        <custom-navbar current-page="php-hello-world"></custom-navbar>
+    </header>
+    <h1><?php echo "Hello, World! PHP is working!"; ?></h1>
+    <p>This is a basic example of mixing HTML with PHP.</p>
+</body>
+</html>
