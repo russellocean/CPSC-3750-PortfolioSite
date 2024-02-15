75fd42a0fca8c1934c7ae9350f9ee8bf1c92ad96
Russell Welch <russellwelch17@gmail.com>
2024-02-15 13:08:14 -0500

    Added Group Events project

--- src/navbar/navbar-component.js
+++ src/navbar/navbar-component.js
@@ -19,6 +19,7 @@ class NavbarComponent extends HTMLElement {
                 <a href="/src/PHPHelloWorld/helloworld.php" data-page="php-hello-world">PHP HELLO WORLD</a>
                 <a href="/src/JavascriptSort/sort.html" data-page="javascript-sort">JAVASCRIPT SORT</a>
                 <a href="/src/MovingButtons/ButtonMove.html" data-page="button-move">MOVING BUTTONS</a>
+                <a href="/src/GroupEvents/GroupEvents.html" data-page="group-events">GROUP - EVENTS</a>
               </div>
             </li>
             <li><a href="mailto:russellwelch17@gmail.com" data-page="contact">CONTACT</a></li>
