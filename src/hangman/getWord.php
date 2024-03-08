c641374eacd6479be8a6666520b2131d10793fd8
Russell Welch <russellwelch17@gmail.com>
2024-03-08 10:48:57 -0500

    Shuffle words array to select a random word

--- src/hangman/getWord.php
+++ src/hangman/getWord.php
@@ -7,6 +7,7 @@ if (file_exists($filePath)) {
    $words = file($filePath, FILE_IGNORE_NEW_LINES | FILE_SKIP_EMPTY_LINES);
    if (!empty($words)) {
        // Randomly select a word from the file
+       shuffle($words);
        $selectedWord = trim($words[array_rand($words)]);
        // Send the selected word back to the client
        echo json_encode(['word' => $selectedWord]);
