443a48fe7d91d25a97baf8db5caca852228f02d8
Russell Welch <russellwelch17@gmail.com>
2024-03-09 12:28:16 -0500

    Finish handlebars

--- src/Handlebars/handlebars.js
+++ src/Handlebars/handlebars.js
@@ -13,7 +13,9 @@ btn.addEventListener("click", function () {
   ourRequest.onload = function () {
     if (ourRequest.status >= 200 && ourRequest.status < 400) {
       var ourData = JSON.parse(ourRequest.responseText);
-      renderHTML(ourData);
+      createHTML({ animals: ourData });
+
+      console.log(ourData);
     } else {
       console.log("We connected to the server, but it returned an error.");
     }
@@ -30,33 +32,10 @@ btn.addEventListener("click", function () {
   }
 });
 
-function renderHTML(data) {
-  var htmlString = "";
+function createHTML(data) {
+  var rawTemplate = document.getElementById("animalTemplate").innerHTML;
+  var compiledTemplate = Handlebars.compile(rawTemplate);
+  var ourGeneratedHTML = compiledTemplate(data);
 
-  for (i = 0; i < data.length; i++) {
-    htmlString +=
-      "<p>" + data[i].name + " is a " + data[i].species + " that likes to eat ";
-
-    for (ii = 0; ii < data[i].foods.likes.length; ii++) {
-      if (ii == 0) {
-        htmlString += data[i].foods.likes[ii];
-      } else {
-        htmlString += " and " + data[i].foods.likes[ii];
-      }
-    }
-
-    htmlString += " and dislikes ";
-
-    for (ii = 0; ii < data[i].foods.dislikes.length; ii++) {
-      if (ii == 0) {
-        htmlString += data[i].foods.dislikes[ii];
-      } else {
-        htmlString += " and " + data[i].foods.dislikes[ii];
-      }
-    }
-
-    htmlString += ".</p>";
-  }
-
-  animalContainer.insertAdjacentHTML("beforeend", htmlString);
+  animalContainer.innerHTML = ourGeneratedHTML;
 }
