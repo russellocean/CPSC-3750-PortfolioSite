0aea7be6f46df5b5943a49da1fbae2cf58d7e057
Russell Welch <russellwelch17@gmail.com>
2024-03-16 16:46:45 -0400

    Fix case sensitivity in LiveSearch link

--- src/navbar/navbar-component.js
+++ src/navbar/navbar-component.js
@@ -30,7 +30,7 @@ class NavbarComponent extends HTMLElement {
               <a href="/src/Handlebars/handlebars.html" data-page="handlebars">Handlebars</a>
               <a href="/src/AudioChanger/AudioChanger.html" data-page="audio-changer">Audio Changer</a>
               <a href="/src/zipcodes/index.html" data-page="zipcodes">Zipcodes</a> 
-              <a href="/src/LiveSearch/LiveSearch.html" data-page="livesearch">Live Search</a>
+              <a href="/src/LiveSearch/livesearch.html" data-page="livesearch">Live Search</a>
             </div>
           </li>
           <li><a href="mailto:russellwelch17@gmail.com" data-page="contact">Contact</a></li>
