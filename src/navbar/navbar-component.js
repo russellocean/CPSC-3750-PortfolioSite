640df298f80dfa86e1c042b0a07eb1c5d5d1630e
russellocean <russellwelch17@gmail.com>
2024-03-30 18:35:50 -0400

    DMMT Examples complete

--- src/navbar/navbar-component.js
+++ src/navbar/navbar-component.js
@@ -32,6 +32,7 @@ class NavbarComponent extends HTMLElement {
               <a href="/src/zipcodes/index.html" data-page="zipcodes">Zipcodes</a> 
               <a href="/src/LiveSearch/livesearch.html" data-page="livesearch">Live Search</a>
               <a href="/src/PHPFile/numberChecker.html" data-page="php-file">PHP File</a>
+              <a href="/src/DMMTExamples/DMMTExamples.html" data-page="dmmt-examples">DMMT Examples</a>
             </div>
           </li>
           <li><a href="mailto:russellwelch17@gmail.com" data-page="contact">Contact</a></li>
